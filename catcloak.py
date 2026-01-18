6bf2b0e68c2f26f5347a96f1fec4aa095d63264d
Matteo Cacciola <matteo.cacciola@gmail.com>
2026-01-19 00:25:01 +0100

    updated according to the enterprise CheshireCat

--- catcloak.py
+++ catcloak.py
@@ -1,9 +1,9 @@
-from cat.mad_hatter.decorators import hook
-from cat.factory.auth_handler import AuthHandlerConfig
-from cat.factory.custom_auth_handler import BaseAuthHandler
-from cat.auth.permissions import (
-    AuthPermission, AuthResource, AuthUserInfo, get_base_permissions
-)
+from collections import defaultdict
+
+from starlette.requests import HTTPConnection
+
+from cat import AuthResource, AuthPermission, AuthHandlerConfig, BaseAuthHandler, hook
+from cat.auth.permissions import AuthUserInfo, get_base_permissions
 from cat.log import log
 
 from pydantic import ConfigDict, Field
@@ -15,12 +15,10 @@ from time import time
 
 @hook(priority=0)
 def factory_allowed_auth_handlers(allowed: List[AuthHandlerConfig], cat) -> List:
-    allowed.append(KeycloakAuthHandlerConfig)
-    return allowed
+    return allowed + [KeycloakAuthHandlerConfig]
 
 
 class KeycloakAuthHandler(BaseAuthHandler):
-
     def __init__(self, **config):
         self.user_mapping = config.get("user_mapping", {})
         self.permission_mapping = config.get("permission_mapping", {})
@@ -34,35 +32,49 @@ class KeycloakAuthHandler(BaseAuthHandler):
         self.token_cache = TTLCache(maxsize=1000, ttl=300)
         self.user_info_cache = TTLCache(maxsize=1000, ttl=300)
 
+    def extract_user_id_http(self, request: HTTPConnection) -> str | None:
+        pass
+
+    def extract_user_id_websocket(self, request: HTTPConnection) -> str | None:
+        pass
+
     def authorize_user_from_jwt(
-            self, token: str, auth_resource: AuthResource, auth_permission: AuthPermission
+        self,
+        token: str,
+        auth_resource: AuthResource,
+        auth_permission: AuthPermission,
+        key_id: str,
     ) -> AuthUserInfo | None:
         try:
+            # Returns cached user if token valid and authorized
+            if token not in self.token_cache:
+                token_info = self.keycloak_openid.decode_token(token)
 
-            if token in self.token_cache:
-                token_info, expiration = self.token_cache[token]
-                if time() < expiration:
-                    user_info = self.user_info_cache.get(token)
-                    if user_info and self.has_permission(user_info, auth_resource, auth_permission):
-                        return user_info
+                expiration = token_info['exp']
+                self.token_cache[token] = (token_info, expiration)
 
-            token_info = self.keycloak_openid.decode_token(token)
+                user_info = self.map_user_data(token_info)
+                self.map_permissions(token_info, user_info)
 
-            expiration = token_info['exp']
-            self.token_cache[token] = (token_info, expiration)
+                log.debug(f"User info: {user_info}")
 
-            user_info = self.map_user_data(token_info)
-            self.map_permissions(token_info, user_info)
+                self.user_info_cache[token] = user_info
 
-            log.debug(f"User info: {user_info}")
+                if not self.permission_mapping:
+                    user_info.permissions = get_base_permissions()
+                    return user_info
 
-            self.user_info_cache[token] = user_info
+                if self.has_permission(user_info, auth_resource, auth_permission):
+                    return user_info
 
-            if not self.permission_mapping:
-                user_info.permissions = get_base_permissions()
-                return user_info
+                return None
 
-            if self.has_permission(user_info, auth_resource, auth_permission):
+            token_info, expiration = self.token_cache[token]
+            if time() >= expiration:
+                return None
+
+            user_info = self.user_info_cache.get(token)
+            if user_info and self.has_permission(user_info, auth_resource, auth_permission):
                 return user_info
             return None
         except Exception as e:
@@ -70,16 +82,23 @@ class KeycloakAuthHandler(BaseAuthHandler):
             return None
 
     def authorize_user_from_key(
-            self, protocol: Literal["http", "websocket"], user_id: str, api_key: str, auth_resource: AuthResource,
-            auth_permission: AuthPermission
+        self,
+        request: HTTPConnection,
+        protocol: Literal["http", "websocket"],
+        api_key: str,
+        auth_resource: AuthResource,
+        auth_permission: AuthPermission,
+        key_id: str,
     ) -> AuthUserInfo | None:
         log.warning("KeycloakAuthHandler does not support API keys.")
         return None
 
     def map_user_data(self, token_info: Dict[str, Any]) -> AuthUserInfo:
-        extra = {key: self.get_nested_value(token_info, path)
-                 for key, path in self.user_mapping.items()
-                 if key not in ["id", "name", "roles"]}
+        extra = {
+            key: self.get_nested_value(token_info, path)
+            for key, path in self.user_mapping.items()
+            if key not in ["id", "name", "roles"]
+        }
 
         return AuthUserInfo(
             id=self.get_nested_value(token_info, self.user_mapping.get("id", "sub")),
@@ -97,24 +116,24 @@ class KeycloakAuthHandler(BaseAuthHandler):
             user_info.permissions = self.kc_permissions[roles_key]
             return
 
-        permissions = {}
+        permissions = defaultdict(set)
         for role in kc_roles:
-            if role in self.permission_mapping:
-                for resource, perms in self.permission_mapping[role].items():
-                    if resource not in permissions:
-                        permissions[resource] = set()
-                    permissions[resource].update(perms)
+            for resource, perms in self.permission_mapping.get(role, {}).items():
+                permissions[resource].update(perms)
 
         permissions = {resource: list(perms) for resource, perms in permissions.items()}
         self.kc_permissions[roles_key] = permissions
         user_info.permissions = permissions
 
-    def has_permission(self, user_info: AuthUserInfo, auth_resource: AuthResource,
-                       auth_permission: AuthPermission) -> bool:
+    @staticmethod
+    def has_permission(
+        user_info: AuthUserInfo, auth_resource: AuthResource, auth_permission: AuthPermission
+    ) -> bool:
         user_permissions = user_info.permissions.get(auth_resource.value, [])
         if auth_permission.value not in user_permissions:
             log.error(
-                f"User {user_info.id} does not have permission to access {auth_resource.value} with {auth_permission.value}")
+                f"User {user_info.id} does not have permission to access {auth_resource.value} with {auth_permission.value}"
+            )
             return False
         return True
 
@@ -127,16 +146,12 @@ class KeycloakAuthHandler(BaseAuthHandler):
 
 
 class KeycloakAuthHandlerConfig(AuthHandlerConfig):
-    _pyclass: Type = KeycloakAuthHandler
-
     server_url: str = Field(..., description="The URL of the Keycloak server.")
     realm: str = Field(..., description="The realm to use.")
     client_id: str = Field(..., description="The client ID to use.")
     client_secret: str = Field(..., description="The client secret to use.")
-    user_mapping: Dict[str, str] = Field(..., description="The mapping of user data from the token to the user model.",
-                                         extra={"type": "TextArea"})
-    permission_mapping: Dict[str, Any] = Field(..., description="The mapping of Keycloak roles to Cat permissions.",
-                                               extra={"type": "TextArea"})
+    user_mapping: Dict[str, str] = Field(..., description="The mapping of user data from the token to the user model.")
+    permission_mapping: Dict[str, Any] = Field(..., description="The mapping of Keycloak roles to Cat permissions.")
 
     model_config = ConfigDict(
         json_schema_extra={
@@ -144,3 +159,7 @@ class KeycloakAuthHandlerConfig(AuthHandlerConfig):
             "description": "Delegate auth to a Keycloak instance."
         }
     )
+
+    @classmethod
+    def pyclass(cls) -> Type[BaseAuthHandler]:
+        return KeycloakAuthHandler
